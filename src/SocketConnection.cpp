4d5cb9a656112226f5b80ecfb8d0232c16898e01
danya <danyadavydov@yandex.ru>
2024-07-29 20:56:43 +0300

    [+] Делаю на 1 порт

--- src/SocketConnection.cpp
+++ src/SocketConnection.cpp
@@ -8,9 +8,9 @@
 #include "Sender.h"
 
 
-void SocketConnection::init(int PORT, const char *ip) {
+void SocketConnection::init(int PORT, std::string &ip) {
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        fprintf(stderr, "[ERROR] Socket creation failed\n");
+        std::cerr << "socket creation failed" << std::endl;
         exit(EXIT_FAILURE);
     }
 
@@ -18,21 +18,21 @@ void SocketConnection::init(int PORT, const char *ip) {
     memset(&cliaddr, 0, sizeof(cliaddr));
 
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
-//    servaddr.sin_addr.s_addr = inet_addr(ip);
     servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
 
     if (bind(sockfd, (const struct sockaddr *) &servaddr,
              sizeof(servaddr)) < 0) {
-        fprintf(stderr, "[ERROR] Bind failed\n");
+        std::cerr << "bind failed" << std::endl;
+        close(sockfd);
         exit(EXIT_FAILURE);
     }
 
 }
 
 void SocketConnection::send(const char *data) {
-    sendto(sockfd, data, strlen(data), 0, (const struct sockaddr *)&servaddr, sizeof(servaddr));
-    fprintf(stdout, "Message sent\n");
+    sendto(sockfd, data, strlen(data), 0, (const struct sockaddr *)&cliaddr, sizeof(cliaddr));
+    std::cout << "Message sent" << std::endl;
 }
 
 void SocketConnection::receive(char * buff) {
